0f232885767ac542ba3fcee49bbedcba8c7e75e7
Nathan Worden <worden.nathan@gmail.com>
2018-10-24 19:12:18 -0500

    updated exercise02.rb and added exercise03.rb

--- 01. The Basics/exercise02.rb
+++ 01. The Basics/exercise02.rb
@@ -3,7 +3,8 @@ hundreds = 4935 % 1000 / 100
 tens = 4935 % 100 / 10
 ones = 4935 % 10
 
-puts thousands
-puts hundreds
-puts tens
-puts ones
+puts "number to slice up: 4935"
+puts "thousands = 4935 / 1000: #{thousands}"
+puts "hundreds = 4935 % 1000 / 100: #{hundreds}"
+puts "tens = 4935 % 100 / 10: #{tens}"
+puts "ones = 4935 % 10: #{ones}"
---
+++ 01. The Basics/exercise03.rb
@@ -0,0 +1,12 @@
+movie_titles = {"Star Wars" => 1975,
+                  "Monster's Inc." => 2005,
+                  "E.T." => 1993,
+                  "John Wick" => 2015,
+                  "Jurrasic Park" => 1979}
+
+
+puts movie_titles["Star Wars"]
+puts movie_titles["Monster's Inc."]
+puts movie_titles["E.T." ]
+puts movie_titles["John Wick"]
+puts movie_titles["Jurrasic Park"]
