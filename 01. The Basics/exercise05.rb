65c42e05b4a411f3ce7195a96b85a958ff7d05a7
Nathan Worden <worden.nathan@gmail.com>
2018-10-24 19:52:37 -0500

    added exercise05.rb

---
+++ 01. The Basics/exercise05.rb
@@ -0,0 +1,9 @@
+factorial_5 = 5 * 4 * 3 * 2 * 1
+factorial_6 = 6 * 5 * 4 * 3 * 2 * 1
+factorial_7 = 7 * 6 * 5 * 4 * 3 * 2 * 1
+factorial_8 = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts "factorial_5 #{factorial_5}"
+puts "factorial_6 #{factorial_6}"
+puts "factorial_7 #{factorial_7}"
+puts "factorial_8 #{factorial_8}"
