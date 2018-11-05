82548f81c6bd51739123f3b051b850bd04a8db0b
Nathan Worden <worden.nathan@gmail.com>
2018-11-05 06:55:26 -0600

    modied 08.exercise8.rb

--- 07. Hashes/08.exercise8.rb
+++ 07. Hashes/08.exercise8.rb
@@ -1,6 +1,6 @@
 # If you see this error, what do you suspect is the most likely problem?
 
-# NoMethodError: undefined method `keys' for Array
+NoMethodError: undefined method `keys' for Array
 
 
 
